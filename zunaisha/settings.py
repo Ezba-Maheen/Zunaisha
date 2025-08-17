670c7334848c013883dad3af2e5d191c4c5bfb9d
Ezba-Maheen <ezbamaheen@gmail.com>
2025-08-17 00:40:35 -0700

    hn

--- zunaisha/settings.py
+++ zunaisha/settings.py
@@ -11,10 +11,7 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
-if not SECRET_KEY:
-    raise ImproperlyConfigured("SECRET_KEY environment variable must be set.")
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG_VALUE', 'False').lower() == 'true'
